2af2db0cfbfdfafae7427886e3764f27bb930bf1
keerthana <meenigakeerthana@gmail.com>
2023-12-13 22:54:20 +0530

    index.java

---
+++ index.java
@@ -0,0 +1,12 @@
+
+public class studentinformation
+public static void main(String[]args)
+{
+	print Student Info();
+}
+{
+	String collegeName="Aits";
+	String studentName="keerthi";
+	system.out.println("collegeName:"+collegeName);
+	system.out.println("studentName:+studentName);
+}
